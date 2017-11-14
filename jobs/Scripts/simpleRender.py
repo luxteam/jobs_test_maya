ca1b2bbfa69c13df68290c110b4bacbd85358722
Andrey Alekseevich <magister.demens@gmail.com>
2017-11-14 20:24:12 +0300

    simplify refactor

--- jobs/Scripts/simpleRender.py
+++ jobs/Scripts/simpleRender.py
@@ -1,10 +1,7 @@
 import argparse
-import sys, os
-import time
+import os
 import subprocess
-import multiprocessing
 import psutil
-import ctypes
 import json
 from shutil import copyfile
 
@@ -31,30 +28,10 @@ def get_windows_titles():
     return titles
 
 
-def _late_kill(pid, delay, real_rc=None):
-    proc = psutil.Process(pid)
-    titles = []
-    while True:
-        try:
-            proc.wait(delay)
-        except psutil.TimeoutExpired as e:
-            titles = get_windows_titles()
-            if "maya" in titles:
-                print("fatal error")
-                if real_rc:
-                    real_rc.value = -1024
-                for child in reversed(proc.children(recursive=True)):
-                    child.terminate()
-                proc.terminate()
-                return
-        else:
-            return
-
-
 def createArgsParser():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--stage_report',required=True)
+    parser.add_argument('--stage_report', required=True)
     parser.add_argument('--tool', required=True, metavar="<path>")
     parser.add_argument('--tests', required=True)
     parser.add_argument('--gpumode', required=True)
@@ -91,7 +68,8 @@ def main():
         return 1
 
     try:
-        with open(os.path.join(os.path.dirname(sys.argv[0]), 'template.mel')) as f:
+        # with open(os.path.join(os.path.dirname(sys.argv[0]), 'template.mel')) as f:
+        with open(os.path.join(os.path.dirname(__file__), 'template.mel')) as f:
             script_template = f.read()
     except OSError as e:
         stage_report[0]['status'] = 'ERROR'
@@ -109,14 +87,14 @@ def main():
     with open(melScriptFile, 'w') as f:
         f.write(melScript)
 
-    copyfile(os.path.join(os.path.dirname(__file__), 'common.mel'), os.path.join(args.output, 'maya_benchmark_common.mel'))
+    copyfile(os.path.join(os.path.dirname(__file__), 'common.mel'),
+             os.path.join(args.output, 'maya_benchmark_common.mel'))
 
     cmdRun = '''
-    set MAYA_CMD_FILE_OUTPUT=%cd%/scriptEditorTrace.txt
+    set MAYA_CMD_FILE_OUTPUT=%cd%/scriptEditorTrace.txt 
     set MAYA_SCRIPT_PATH=%cd%;%MAYA_SCRIPT_PATH%
-    "{tool}" -command "source script.mel; evalDeferred -lp (mayaBenchmark({{{gpumode}}}));"
-    '''\
-    .format(tool = args.tool, gpumode = args.gpumode)
+    "{tool}" -command "source script.mel; evalDeferred -lp (mayaBenchmark({{{gpumode}}}));"''' \
+        .format(tool=args.tool, gpumode=args.gpumode)
 
     try:
         cmdScriptPath = os.path.join(args.output, 'script.bat')
@@ -128,34 +106,19 @@ def main():
         stage_report[1]['log'].append('OSError while write mel script. ' + str(e))
         return 1
 
-    os.chdir(melScriptPath)
+    os.chdir(args.output)
 
-    started = time.time()
-    child = subprocess.Popen(cmdScriptPath, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    p = psutil.Popen(os.path.join(args.output, 'script.bat'), stdout=subprocess.PIPE)
 
-    real_rc = multiprocessing.Value('i', 0)
-    monitor = multiprocessing.Process(target=_late_kill, args=(child.pid, 10, real_rc))
-    monitor.start()
+    try:
+        rc = p.wait(timeout=2000)
+    except psutil.TimeoutExpired as err:
+        sub_skip = -1
+        for child in reversed(p.children(recursive=True)):
+            child.terminate()
+        p.terminate()
 
     sub_skip = 0
-
-    while True:
-        output = child.stdout.readline()
-        if output == b'' and child.poll() != None:
-            break
-        if output != b'' and output != b'\r\n':
-            output = output.decode("UTF-8")
-            output_noeol = str(output).replace('\r', '')
-            output_noeol = str(output_noeol).replace('\n', '')
-            print(output_noeol)
-            if "not recognized as an internal or external command" in output_noeol:
-                sub_skip = 1
-            if "The system cannot find the path specified" in output_noeol:
-                sub_skip = 1
-
-    rc = ctypes.c_long(child.poll()).value
-    print('RC', rc)
-    print(type(rc))
 
     if sub_skip:
         rc = 2
@@ -171,17 +134,11 @@ def main():
         stage_report[0]['status'] = 'FAILED'
         stage_report[1]['log'].append('subprocess FAILED')
 
-    monitor.terminate()
-
-    if real_rc.value != 0:
-        rc = real_rc.value
-        stage_report[0]['status'] = 'FAILED'
-        stage_report[1]['log'].append('subprocess was terminated by monitor')
-
     with open(os.path.join(args.output, args.stage_report), 'w') as file:
         json.dump(stage_report, file, indent=' ')
 
     return rc
+
 
 if __name__ == "__main__":
     rc = main()
