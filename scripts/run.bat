2f97b3d28e85a1e52e6edd5eb4f14ef8cae49a06
Andrey Alekseevich <magister.demens@gmail.com>
2017-11-17 15:59:22 +0300

    added env parametrization

--- scripts/run.bat
+++ scripts/run.bat
@@ -1,2 +1,6 @@
 set PATH=c:\python35\;c:\python35\scripts\;%PATH%
+set HWProfiles=CPU
+set TestsFilter=small
+set Tool=2017
+
 python ..\..\jobs_launcher\executeTests.py --tests_root ..\jobs --work_root ..\Results --work_dir maya
