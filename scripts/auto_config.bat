d055443ef88c7e88fcf55607306509bf4ab16a2e
Radeon Prorender <radeonprorender.buildmaster@gmail.com>
2017-12-13 19:21:06 +0100

    Fixed mel scripts for CPU only case

--- scripts/auto_config.bat
+++ scripts/auto_config.bat
@@ -1,3 +1,3 @@
 set MAYA_SCRIPT_PATH=%cd%;%MAYA_SCRIPT_PATH%
 
-"C:\Program Files\Autodesk\Maya2017\bin\maya.exe" -batch -command "source auto_config.mel; auto_config();"
+"C:\Program Files\Autodesk\Maya2017\bin\mayabatch.exe" -command "source auto_config.mel; auto_config();"
