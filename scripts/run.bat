09ef21d911cbbc5d8c1f7a35e6e8c703fc4405d7
Kravchenko Andrey <epozine@inbox.ru>
2018-02-26 17:26:03 +0300

    Fix test lists

--- scripts/run.bat
+++ scripts/run.bat
@@ -5,6 +5,6 @@ set TEST_PACKAGE=%3
 if "%RENDER_DEVICE%" EQU "" set RENDER_DEVICE=gpu
 if "%TESTS_FILTER%" EQU "" set TESTS_FILTER=full
 
-python ..\jobs_launcher\executeTests.py --test_package --tests_root ..\jobs --work_root ..\Work\Results --work_dir Maya --cmd_variables Tool "C:\Program Files\Autodesk\Maya2017\bin\maya.exe" RenderDevice %RENDER_DEVICE% TestsFilter %TESTS_FILTER% ResPath "c:\TestResources\MayaAssets"
+python ..\jobs_launcher\executeTests.py --test_package IES_Light_Test --tests_root ..\jobs --work_root ..\Work\Results --work_dir Maya --cmd_variables Tool "C:\Program Files\Autodesk\Maya2017\bin\maya.exe" RenderDevice %RENDER_DEVICE% TestsFilter %TESTS_FILTER% ResPath "c:\TestResources\MayaAssets"
 
 pause
