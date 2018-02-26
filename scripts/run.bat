7aed0487cec3431fc44c8d16ce9a25d246baa3d9
Kravchenko Andrey <epozine@inbox.ru>
2018-02-26 18:23:30 +0300

    Fix render device

--- scripts/run.bat
+++ scripts/run.bat
@@ -2,7 +2,7 @@ set PATH=c:\python35\;c:\python35\scripts\;%PATH%
 set RENDER_DEVICE=%1
 set TESTS_FILTER=%2
 set TEST_PACKAGE=%3
-if "%RENDER_DEVICE%" EQU "" set RENDER_DEVICE=gpu
+if "%RENDER_DEVICE%" EQU "" set RENDER_DEVICE=1
 if "%TESTS_FILTER%" EQU "" set TESTS_FILTER=full
 
 python ..\jobs_launcher\executeTests.py --test_package IES_Light_Test --tests_root ..\jobs --work_root ..\Work\Results --work_dir Maya --cmd_variables Tool "C:\Program Files\Autodesk\Maya2017\bin\maya.exe" RenderDevice %RENDER_DEVICE% TestsFilter %TESTS_FILTER% ResPath "c:\TestResources\MayaAssets"
