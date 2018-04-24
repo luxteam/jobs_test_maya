fc1716e9e3ef9c082b6fc3d813a3d12bb39a99c1
Kravchenko Andrey <epozine@inbox.ru>
2018-04-24 12:06:51 +0300

    fixes in maya

--- scripts/run.bat
+++ scripts/run.bat
@@ -4,7 +4,7 @@ set TESTS_FILTER=%2
 set TEST_PACKAGE=%3
 if "%RENDER_DEVICE%" EQU "" set RENDER_DEVICE=gpu
 if "%TESTS_FILTER%" EQU "" set TESTS_FILTER=full
-if "%TEST_PACKAGE%" EQU "" set TEST_PACKAGE=""
+if "%TEST_PACKAGE%" EQU "" set TEST_PACKAGE=PR
 
 python ..\jobs_launcher\executeTests.py --file_filter %TEST_PACKAGE% --tests_root ..\jobs --work_root ..\Work\Results --work_dir Maya --cmd_variables Tool "C:\Program Files\Autodesk\Maya2017\bin\maya.exe" RenderDevice %RENDER_DEVICE% TestsFilter %TESTS_FILTER% ResPath "C:\TestResources\MayaAssets" PassLimit 1 rx 0 ry 0
 
