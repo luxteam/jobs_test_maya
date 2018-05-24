f1d4c50a8de3c63a7e35ac0c6f3f5551e683ba43
Alekseevich Andrey <MagisterDemens@users.noreply.github.com>
2018-05-24 21:41:28 +0300

    Update run.bat

--- scripts/run.bat
+++ scripts/run.bat
@@ -6,4 +6,4 @@ if "%RENDER_DEVICE%" EQU "" set RENDER_DEVICE=gpu
 if "%TESTS_FILTER%" EQU "" set TESTS_FILTER=full
 if "%TEST_PACKAGE%" EQU "" set TEST_PACKAGE=smoke
 
-python ..\jobs_launcher\executeTests.py --file_filter %TEST_PACKAGE% --tests_root ..\jobs --work_root ..\Work\Results --work_dir Maya --cmd_variables Tool "C:\Program Files\Autodesk\Maya2017\bin\maya.exe" RenderDevice %RENDER_DEVICE% TestsFilter %TESTS_FILTER% ResPath "C:\TestResources\MayaAssets" PassLimit 100 rx 0 ry 0
+python ..\jobs_launcher\executeTests.py --file_filter %TEST_PACKAGE% --tests_root ..\jobs --work_root ..\Work\Results --work_dir Maya --cmd_variables Tool "C:\Program Files\Autodesk\Maya2017\bin\maya.exe" RenderDevice %RENDER_DEVICE% TestsFilter %TESTS_FILTER% ResPath "C:\TestResources\MayaAssets" PassLimit 10 rx 0 ry 0
