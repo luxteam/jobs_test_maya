3147f84ec7df0b377c26ca4f9ce545996da03909
Andrey Alekseevich <magister.demens@gmail.com>
2018-07-05 16:13:23 +0300

    added multi-filters

--- scripts/run.bat
+++ scripts/run.bat
@@ -1,9 +1,8 @@
 set PATH=c:\python35\;c:\python35\scripts\;%PATH%
 set RENDER_DEVICE=%1
-set TESTS_FILTER=%2
-set TEST_PACKAGE=%3
+set FILE_FILTER=%2
+set TESTS_FILTER="%3"
 if "%RENDER_DEVICE%" EQU "" set RENDER_DEVICE=gpu
-if "%TESTS_FILTER%" EQU "" set TESTS_FILTER=full
-if "%TEST_PACKAGE%" EQU "" set TEST_PACKAGE=smoke
+if "%FILE_FILTER%" EQU "" set FILE_FILTER=smoke
 
-python ..\jobs_launcher\executeTests.py --file_filter %TEST_PACKAGE% --tests_root ..\jobs --work_root ..\Work\Results --work_dir Maya --cmd_variables Tool "C:\Program Files\Autodesk\Maya2017\bin\maya.exe" RenderDevice %RENDER_DEVICE% TestsFilter %TESTS_FILTER% ResPath "C:\TestResources\MayaAssets" PassLimit 50 rx 0 ry 0
+python ..\jobs_launcher\executeTests.py --test_filter %TESTS_FILTER% --file_filter %FILE_FILTER% --tests_root ..\jobs --work_root ..\Work\Results --work_dir Maya --cmd_variables Tool "C:\Program Files\Autodesk\Maya2017\bin\maya.exe" RenderDevice %RENDER_DEVICE% ResPath "C:\TestResources\MayaAssets" PassLimit 50 rx 0 ry 0
