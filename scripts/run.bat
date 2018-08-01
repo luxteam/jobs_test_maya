62218cf91416adaf27086998b60170fbf5291862
Andrey Alekseevich <magister.demens@gmail.com>
2018-08-01 17:33:50 +0300

    add split execution

--- scripts/run.bat
+++ scripts/run.bat
@@ -5,4 +5,4 @@ set TESTS_FILTER="%3"
 if "%RENDER_DEVICE%" EQU "" set RENDER_DEVICE=gpu
 if "%FILE_FILTER%" EQU "" set FILE_FILTER=smoke
 
-python ..\jobs_launcher\executeTests.py --test_filter %TESTS_FILTER% --file_filter %FILE_FILTER% --tests_root ..\jobs --work_root ..\Work\Results --work_dir Maya --cmd_variables Tool "C:\Program Files\Autodesk\Maya2017\bin\maya.exe" RenderDevice %RENDER_DEVICE% ResPath "C:\TestResources\MayaAssets" PassLimit 50 rx 0 ry 0
+python ..\jobs_launcher\executeTests.py --split_execution %4 --test_filter %TESTS_FILTER% --file_filter %FILE_FILTER% --tests_root ..\jobs --work_root ..\Work\Results --work_dir Maya --cmd_variables Tool "C:\Program Files\Autodesk\Maya2017\bin\maya.exe" RenderDevice %RENDER_DEVICE% ResPath "C:\TestResources\MayaAssets" PassLimit 50 rx 0 ry 0
