a34b96127ce8c00a1ae0f575fd3ce7de21759f20
Alekseevich Andrey <11044999+MagisterDemens@users.noreply.github.com>
2019-03-15 20:41:11 +0300

    Update build_rpr_cache.bat

--- scripts/build_rpr_cache.bat
+++ scripts/build_rpr_cache.bat
@@ -1,3 +1,3 @@
 set PYTHONPATH=%CD%\..\jobs\Scripts;%PYTHONPATH%
 set MAYA_SCRIPT_PATH=%CD%\..\jobs\Scripts;%MAYA_SCRIPT_PATH%
-"C:\Program Files\Autodesk\Maya2018\bin\maya.exe" -command "python(\"import cache_building\")"
+"C:\Program Files\Autodesk\Maya2017\bin\maya.exe" -command "python(\"import cache_building\")"
