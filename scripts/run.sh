9336d233627c1ab2b12ae904fbed9136853cfc8f
Andrey Alekseevich <magister.demens@gmail.com>
2019-08-05 21:23:36 +0300

    Commented corruped tests

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python ../jobs_launcher/executeTests.py --test_filter "" --file_filter smoke --tests_root ../jobs --work_root ../Work/Results --work_dir Maya --cmd_variables Tool "maya" RenderDevice "gpu" ResPath "$CIS_TOOLS/../TestResources/MayaAssets" PassLimit 5 rx 0 ry 0 SPU 10
+python ../jobs_launcher/executeTests.py --test_filter "" --file_filter $1 --tests_root ../jobs --work_root ../Work/Results --work_dir Maya --cmd_variables Tool "maya" RenderDevice "gpu" ResPath "$CIS_TOOLS/../TestResources/MayaAssets" PassLimit 5 rx 0 ry 0 SPU 10
