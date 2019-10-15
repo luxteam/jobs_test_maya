50b0bdee02d3ccb5677b5419bd859bd8c6622203
Andrey Alekseevich <magister.demens@gmail.com>
2019-10-15 22:47:01 +0300

    try without relaunch

--- jobs/Scripts/simpleRender.py
+++ jobs/Scripts/simpleRender.py
@@ -170,12 +170,10 @@ def main(args, startFrom, lastStatus):
     p = psutil.Popen(cmdScriptPath, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     rc = -1
 
-    core_config.main_logger.info("go to infinity")
+    core_config.main_logger.info("Maya running...")
     while True:
         try:
-            p.communicate(timeout=10)
-            core_config.main_logger.info("")
-
+            p.communicate(timeout=20)
         except (psutil.TimeoutExpired, subprocess.TimeoutExpired) as err:
             fatal_errors_titles = ['maya', 'Student Version File', 'Radeon ProRender Error', 'Script Editor',
                 'Autodesk Maya 2017 Error Report', 'Autodesk Maya 2017 Error Report', 'Autodesk Maya 2017 Error Report',
@@ -188,11 +186,14 @@ def main(args, startFrom, lastStatus):
                 try:
                     error_screen = pyscreenshot.grab()
                     error_screen.save(os.path.join(args.output, 'error_screenshot.jpg'))
-                except:
+                except Exception as screen_err:
                     core_config.main_logger.warning("Exception while try to screen error message")
+
+                core_config.main_logger.info("Kill maya ...")
                 for child in reversed(p.children(recursive=True)):
                     child.kill()
                 p.kill()
+                core_config.main_logger.info("Killed")
                 break
         else:
             rc = 0
@@ -203,65 +204,11 @@ def main(args, startFrom, lastStatus):
 
 if __name__ == "__main__":
 
-    args = createArgsParser().parse_args()
-
     try:
         os.makedirs(args.output)
     except OSError as e:
         pass
 
-    def getJsonCount():
-        return len(list(filter(lambda x: x.endswith('RPR.json'), os.listdir(args.output))))
+    args = createArgsParser().parse_args()
+    exit(main(args, 0, 0))
 
-    def totalCount():
-        try:
-            with open(os.path.join(os.path.dirname(__file__),  args.template)) as f:
-                script_template = f.read()
-            return len(script_template.split("@")) - 1  # -1 because first element is "" (split)
-        except OSError as e:
-            return -1
-
-    total_count = totalCount()
-    fail_count = 0
-    current_test = 1  # start from 1st test
-    last_status = 0  # 0 - success status
-    it = 0
-
-    while current_test <= total_count:
-
-        it += 1
-
-        with open(os.path.join(args.output, 'log_status.txt'), 'a') as f:
-            f.write("Iter:" + str(it) + " | current test: " + str(current_test) + " | fail count: " +
-                    str(fail_count) + " | last_status: " + str(last_status) + " | json: " +
-                    str(getJsonCount()) + " | total count: " + str(total_count) + "\n")
-
-        if last_status and fail_count == 3:
-            rc = main(args, current_test, "fail")  # Start from n+1 test. n - fail.
-        elif last_status and fail_count == -1:
-            rc = main(args, current_test, "last_fail")  # last test - fail.
-        else:
-            rc = main(args, current_test, "ok")  # Start from 1st test (ok - random word)
-
-        if current_test != getJsonCount() + 1:  # count to zero if failes another test
-            fail_count = 0
-
-        last_status = rc
-        if not last_status:
-            if not getJsonCount():
-                rc = main(args, current_test, "no scene")
-            exit(rc)  # finish work. 0 - success status.
-        elif last_status and fail_count == 2:
-            if total_count < getJsonCount() + 2:  # last test failed
-                fail_count = -1
-                current_test = getJsonCount() + 1
-            else:  # not last test failed
-                fail_count += 1
-                current_test = getJsonCount() + 2  # mark as fail test and go to next test
-        elif last_status:
-            if getJsonCount() == total_count:
-                exit(rc)
-            fail_count += 1  # count of failes + 1 (for current test)
-            current_test = getJsonCount() + 1
-
-    exit(0)
