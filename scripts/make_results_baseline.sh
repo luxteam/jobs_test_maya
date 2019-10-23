9cfb5cb0af22a09bbbcc8fb1013624fcb200cda5
Andrey Alekseevich <magister.demens@gmail.com>
2019-10-23 16:03:32 +0300

    paths fix

--- scripts/make_results_baseline.sh
+++ scripts/make_results_baseline.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-set PYTHONPATH=..\jobs_launcher\;%PYTHONPATH%
+set PYTHONPATH=../jobs_launcher/;%PYTHONPATH%
 
-python3 ..\jobs_launcher\common\scripts\generate_baseline.py --results_root ..\Work\Results\Maya --baseline_root ..\Work\Baseline
+python3 ../jobs_launcher/common/scripts/generate_baseline.py --results_root ../Work/Results/Maya --baseline_root ../Work/Baseline
