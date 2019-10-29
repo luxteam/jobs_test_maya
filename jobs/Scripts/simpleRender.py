bf00127f4afc22130167f4a216822aea3a9e165a
Andrei Kravchenko <epozine@inbox.ru>
2019-10-29 19:09:15 +0300

    Add check for maya after killing

--- jobs/Scripts/simpleRender.py
+++ jobs/Scripts/simpleRender.py
@@ -244,9 +244,6 @@ def main(args, startFrom, lastStatus):
                     except psutil.NoSuchProcess:
                         core_config.main_logger.info("Process is killed: {}. Name: {}".format(ch, ch.name()))
 
-                if mayaKiller(kill=True):
-                    core_config.main_logger.error("Maya is alive.")
-
                 try:
                     p.terminate()
                     p.kill()
@@ -259,6 +256,9 @@ def main(args, startFrom, lastStatus):
         else:
             rc = 0
             break
+
+    if mayaKiller(kill=True):
+        core_config.main_logger.error("Maya is alive.")
 
     core_config.main_logger.info("Main func return : {}".format(rc))
     return rc
