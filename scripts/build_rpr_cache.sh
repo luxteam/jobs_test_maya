8bf0fc36823d51eafb14454f3b7cd3f8a0c3f269
Alekseevich Andrey <11044999+MagisterDemens@users.noreply.github.com>
2019-11-07 22:25:21 +0300

    Update build_rpr_cache.sh

--- scripts/build_rpr_cache.sh
+++ scripts/build_rpr_cache.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 export PYTHONPATH=$PWD/../jobs/Scripts:$PYTHONPATH
 export MAYA_SCRIPT_PATH=$PWD/../jobs/Scripts:$MAYA_SCRIPT_PATH
+export MAYA_CMD_FILE_OUTPUT=$PWD/cacheBuilding.log
+
 maya2019 -command "python(\"import cache_building\")"
