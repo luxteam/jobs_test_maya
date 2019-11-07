814c8628b8f169f46d8739f281ecac0ccc286a7c
Alekseevich Andrey <11044999+MagisterDemens@users.noreply.github.com>
2019-11-07 22:24:34 +0300

    Update cache_building.py

--- jobs/Scripts/cache_building.py
+++ jobs/Scripts/cache_building.py
@@ -1,15 +1,35 @@
 import maya.cmds as cmds
 import maya.mel as mel
 
-if not cmds.pluginInfo("RadeonProRender", q=True, loaded=True):
-    cmds.loadPlugin("RadeonProRender")
 
-cmds.sphere(radius=4)
+def main():
+    print("Render simple object for build render cache.")
+    try:
+        if not cmds.pluginInfo("RadeonProRender", q=True, loaded=True):
+            print("Plugin not loaded, try to load...")
+            cmds.loadPlugin("RadeonProRender")
+    except Exception as err:
+        print("Error during plugin load. {}".format(str(err)))
+        cmds.quit(abort=True)
 
-cmds.setAttr("defaultRenderGlobals.currentRenderer", "FireRender", type="string")
-cmds.setAttr("RadeonProRenderGlobals.completionCriteriaSeconds", 1)
-cmds.setAttr("RadeonProRenderGlobals.completionCriteriaIterations", 1)
-cmds.fireRender(waitForItTwo=True)
-mel.eval("renderIntoNewWindow render")
+    print("Plugin has been loaded")
+    try:
+        print("Render sphere with RPR...")
 
-cmds.evalDeferred("cmds.quit(abort=True)")
+        cmds.sphere(radius=4)
+
+        cmds.setAttr("defaultRenderGlobals.currentRenderer", "FireRender", type="string")
+        cmds.setAttr("RadeonProRenderGlobals.completionCriteriaSeconds", 1)
+        cmds.setAttr("RadeonProRenderGlobals.completionCriteriaIterations", 1)
+        cmds.fireRender(waitForItTwo=True)
+        mel.eval("renderIntoNewWindow render")
+        print("Render has been finished")
+    except Exception as err:
+        print("Error during rendering. {}".format(str(err)))
+        cmds.quit(abort=True)
+    finally:
+        print("Quit")
+        cmds.evalDeferred("cmds.quit(abort=True)")
+
+
+cmds.evalDeferred("cache_building.main()")
