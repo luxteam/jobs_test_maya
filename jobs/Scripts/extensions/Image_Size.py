15c5bb45205c82ed8fe0a5116de8822079a8fdc4
kataderon <kataderon@gmail.com>
2019-12-03 14:00:22 +0300

    refactor image size

---
+++ jobs/Scripts/extensions/Image_Size.py
@@ -0,0 +1,5 @@
+def resetAttributes():
+    cmds.setAttr('defaultResolution.dotsPerInch', 72.0)
+    cmds.setAttr('defaultResolution.pixelAspect', 1.0)
+    cmds.setAttr('defaultResolution.deviceAspectRatio', 1.0)
+    cmds.setAttr('defaultResolution.imageSizeUnits', 0)
