8d5ba76480b39ca247d5461ac7765128da77a9ef
Alekseevich Andrey <11044999+MagisterDemens@users.noreply.github.com>
2019-12-09 11:57:39 +0300

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 python3 -m pip install -r ../jobs_launcher/install/requirements.txt
-python3 ../jobs_launcher/executeTests.py --test_filter "$3" --file_filter $2 --tests_root ../jobs --work_root ../Work/Results --work_dir Maya --cmd_variables Tool maya2019 RenderDevice "$1" ResPath "$CIS_TOOLS/../TestResources/MayaAssets" PassLimit 50 rx 0 ry 0 SPU 10
+python3 ../jobs_launcher/executeTests.py --test_filter "$3" --file_filter $2 --tests_root ../jobs --work_root ../Work/Results --work_dir Maya --cmd_variables Tool maya2019 RenderDevice "$1" ResPath "$CIS_TOOLS/../TestResources/MayaAssets" PassLimit 5 rx 0 ry 0 SPU 5
