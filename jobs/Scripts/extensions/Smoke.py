220738c5281ed5825423a9e119cf0e2286a20ff7
kataderon <kataderon@gmail.com>
2019-12-10 19:27:19 +0300

    fix doesn't work

--- jobs/Scripts/extensions/Smoke.py
+++ jobs/Scripts/extensions/Smoke.py
@@ -25,8 +25,8 @@ def setAttribute(volume_attr, file_attr, value):
 
 	return file
 
-try:
-	with open(path.join(WORK_DIR, 'renderTool.log')) as f: 
-		if 'successfully uploaded data to AWS!' in f.read(): 
-			cmds.sysFile(path.join(WORK_DIR, '..', '..', '..', '..', 'jobs', 'Tests', 'pass.jpg'), copy=path.join(WORK_DIR, 'Color', 'MAYA_SM_043.jpg'))
-except:pass
+# try:
+# 	with open(path.join(WORK_DIR, 'renderTool.log')) as f: 
+# 		if 'successfully uploaded data to AWS!' in f.read(): 
+# 			cmds.sysFile(path.join(WORK_DIR, '..', '..', '..', '..', 'jobs', 'Tests', 'pass.jpg'), copy=path.join(WORK_DIR, 'Color', 'MAYA_SM_043.jpg'))
+# except:pass
