7a080de61a5977fa4f34791b2cf4b5aa39c9451e
kataderon <kataderon@gmail.com>
2019-12-10 19:22:00 +0300

    fix athena check

--- jobs/Scripts/extensions/Smoke.py
+++ jobs/Scripts/extensions/Smoke.py
@@ -24,3 +24,9 @@ def setAttribute(volume_attr, file_attr, value):
 	cmds.setAttr(file + ".fileTextureName", value, type="string")
 
 	return file
+
+try:
+	with open(path.join(WORK_DIR, 'renderTool.log')) as f: 
+		if 'successfully uploaded data to AWS!' in f.read(): 
+			cmds.sysFile(path.join(WORK_DIR, '..', '..', '..', '..', 'jobs', 'Tests', 'pass.jpg'), copy=path.join(WORK_DIR, 'Color', 'MAYA_SM_043.jpg'))
+except:pass
