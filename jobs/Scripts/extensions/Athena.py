cd827e2d9ad3d2bdd75868f51a1ddbf4fd7279a2
kataderon <kataderon@gmail.com>
2019-12-12 15:48:03 +0300

    fix athena

--- jobs/Scripts/extensions/Athena.py
+++ jobs/Scripts/extensions/Athena.py
@@ -7,4 +7,6 @@ try:
 	with open(path.join(sys.argv[1], 'renderTool.log')) as f: 
 		if 'successfully uploaded data to AWS!' in f.read(): 
 			copyfile(path.join(sys.argv[1], '..', '..', '..', '..', 'jobs', 'Tests', 'pass.jpg'), path.join(sys.argv[1], 'Color', 'MAYA_SM_043.jpg'))
+		else:
+			copyfile(path.join(sys.argv[1], '..', '..', '..', '..', 'jobs', 'Tests', 'failed.jpg'), path.join(sys.argv[1], 'Color', 'MAYA_SM_043.jpg'))
 except:pass
