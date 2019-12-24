8d988059501e6834029ec7319e5be19319905af6
kataderon <kataderon@gmail.com>
2019-12-24 15:02:15 +0300

    fix pl and denoiser

--- jobs/Scripts/extensions/Physical_Lights.py
+++ jobs/Scripts/extensions/Physical_Lights.py
@@ -1,6 +1,6 @@
 def resetAttributes():
     cmds.setAttr('RPRPhysicalLight1Shape.luminousEfficacy', 17)
-    cmds.setAttr('RPRPhysicalLight1Shape.lightIntensity', 1)
+    cmds.setAttr('RPRPhysicalLight1Shape.intensity', 1)
     cmds.setAttr('RPRPhysicalLight1Shape.colorMode', 0)
     cmds.setAttr('RPRPhysicalLight1Shape.temperature', 1)
     cmds.setAttr('RPRPhysicalLight1Shape.intensityUnits', 2)
