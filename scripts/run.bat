964fc46155b07b0899327c5bb2d3afa2ac8448cb
kataderon <kataderon@gmail.com>
2020-01-20 12:44:01 +0300

    fix tool param and add theshold param

--- scripts/run.bat
+++ scripts/run.bat
@@ -7,7 +7,8 @@ set RY=%5
 set SPU=%6
 set ITER=%7
 set THESHLOD=%8
+set TOOL=%9
 
 python -m pip install -r ..\jobs_launcher\install\requirements.txt
 
-python ..\jobs_launcher\executeTests.py --test_filter %TESTS_FILTER% --file_filter %FILE_FILTER% --tests_root ..\jobs --work_root ..\Work\Results --work_dir Maya --cmd_variables Tool "C:\Program Files\Autodesk\Maya2019\bin\maya.exe" RenderDevice %RENDER_DEVICE% ResPath "C:\TestResources\MayaAssets" PassLimit %ITER% rx %RX% ry %RY% SPU %SPU%
+python ..\jobs_launcher\executeTests.py --test_filter %TESTS_FILTER% --file_filter %FILE_FILTER% --tests_root ..\jobs --work_root ..\Work\Results --work_dir Maya --cmd_variables Tool "C:\Program Files\Autodesk\Maya%TOOL%\bin\maya.exe" RenderDevice %RENDER_DEVICE% ResPath "C:\TestResources\MayaAssets" PassLimit %ITER% rx %RX% ry %RY% SPU %SPU% theshlod %THESHLOD%
--- scripts/run.sh
+++ scripts/run.sh
@@ -2,6 +2,12 @@
 RENDER_DEVICE=$1
 FILE_FILTER=$2
 TESTS_FILTER="$3"
+RX=$4
+RY=$5
+SPU=$6
+ITER=$7
+THESHLOD=$8
+TOOL=$9
 
 python3 -m pip install -r ../jobs_launcher/install/requirements.txt
-python3 ../jobs_launcher/executeTests.py --file_filter $FILE_FILTER --test_filter $TESTS_FILTER --tests_root ../jobs --work_root ../Work/Results --work_dir Maya --cmd_variables Tool maya2019 RenderDevice "$RENDER_DEVICE" ResPath "$CIS_TOOLS/../TestResources/MayaAssets" PassLimit 50 rx 0 ry 0 SPU 10
+python3 ../jobs_launcher/executeTests.py --file_filter $FILE_FILTER --test_filter $TESTS_FILTER --tests_root ../jobs --work_root ../Work/Results --work_dir Maya --cmd_variables Tool "maya${TOOL}" RenderDevice "$RENDER_DEVICE" ResPath "$CIS_TOOLS/../TestResources/MayaAssets" PassLimit $ITER rx $RX ry $RY SPU $SPU theshlod $THESHLOD
