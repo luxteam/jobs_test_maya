63c5af06e523543041a706b93b4eb20a9ab0d338
kataderon <kataderon@gmail.com>
2020-02-04 11:57:12 +0300

    Fix version and regexp

--- scripts/build_rpr_cache.sh
+++ scripts/build_rpr_cache.sh
@@ -3,6 +3,6 @@ export PYTHONPATH=$PWD/../jobs/Scripts:$PYTHONPATH
 export MAYA_SCRIPT_PATH=$PWD/../jobs/Scripts:$MAYA_SCRIPT_PATH
 export MAYA_CMD_FILE_OUTPUT=$PWD/cacheBuilding.log
 
-TOOL=${1:-2019}
+TOOL=${1:-2020}
 
 maya${TOOL} -command "python(\"import cache_building\")"
