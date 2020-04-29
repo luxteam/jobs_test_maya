c9df5fc67800b3bc2e5a1a01d99cc655efad6c68
GulDilin <zhenyagurin@gmail.com>
2020-04-29 19:45:12 +0300

    add try block for get env use rbs

--- jobs/Scripts/simpleRender.py
+++ jobs/Scripts/simpleRender.py
@@ -353,7 +353,11 @@ if __name__ == '__main__':
 
 	is_client = None
 	rbs_client = None
-	rbs_use = str2bool(os.getenv('RBS_USE'))
+
+	try:
+        use_rbs = str2bool(os.getenv('RBS_USE'))
+    except Exception as e:
+        print('Exception when getenv RBS USE: {}'.format(str(e)))
 
 	if rbs_use:
 		try:
