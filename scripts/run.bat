ead9c33776dceb0ad00d4d751131aa61ccc55e48
kataderon <kataderon@gmail.com>
2020-05-08 17:17:00 +0300

    fix run bat

--- scripts/run.bat
+++ scripts/run.bat
@@ -1,14 +1,15 @@
 set PATH=c:\python35\;c:\python35\scripts\;%PATH%
-set RENDER_DEVICE=%1
-set FILE_FILTER=%2
-set TESTS_FILTER="%3"
-set RX=%4
-set RY=%5
-set SPU=%6
-set ITER=%7
-set THRESHOLD=%8
-set TOOL=%9
-set ENGINE=%10
+set RENDER_DEVICE=%~1
+set FILE_FILTER=%~2
+set TESTS_FILTER="%~3"
+set RX=%~4
+set RY=%~5
+set SPU=%~6
+set ITER=%~7
+set THRESHOLD=%~8
+set TOOL=%~9
+shift
+set ENGINE=%~9
 
 if not defined RX set RX=0
 if not defined RY set RY=0
@@ -16,7 +17,7 @@ if not defined SPU set SPU=25
 if not defined ITER set ITER=50
 if not defined THRESHOLD set THRESHOLD=0.05
 if not defined TOOL set TOOL=2020
-if not defined TOOL set ENGINE=2
+if not defined ENGINE set ENGINE=2
 
 python -m pip install -r ..\jobs_launcher\install\requirements.txt
 
