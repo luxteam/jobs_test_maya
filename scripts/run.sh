c9c9871d91f454f691f38c279f85a65155a0e5a1
GulDilin <zhenyagurin@gmail.com>
2020-05-09 20:42:04 +0300

    add rbs env vars set to run sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -9,6 +9,23 @@ ITER=${7:-50}
 THRESHOLD=${8:-0.05}
 TOOL=${9:-2020}
 
+shift
+shift
+shift
+shift
+shift
+shift
+shift
+shift
+shift
+
+RBS_BUILD_ID=$1
+RBS_JOB_ID=$2
+RBS_URL=$3
+RBS_ENV_LABEL=$4
+IMAGE_SERVICE_URL=$5
+RBS_USE=$6
+
 python3 -m pip install -r ../jobs_launcher/install/requirements.txt
 
 python3 ../jobs_launcher/executeTests.py --file_filter $FILE_FILTER --test_filter $TESTS_FILTER --tests_root ../jobs --work_root ../Work/Results --work_dir Maya --cmd_variables Tool "maya${TOOL}" RenderDevice "$RENDER_DEVICE" ResPath "$CIS_TOOLS/../TestResources/MayaAssets" PassLimit $ITER rx $RX ry $RY SPU $SPU threshold $THRESHOLD
