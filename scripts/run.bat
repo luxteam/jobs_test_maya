3af384b1a562749cd83314214a1074c492d80759
Daniil Tarasenko <kataderon@gmail.com>
2020-05-26 15:52:49 +0300

    fix args in run.bat (#117)
    Co-authored-by: Tarasenko Daniil <dtarasenko@luxoft.com>

--- scripts/run.bat
+++ scripts/run.bat
@@ -1,15 +1,15 @@
 set PATH=c:\python35\;c:\python35\scripts\;%PATH%
-set RENDER_DEVICE=%~1
-set FILE_FILTER=%~2
-set TESTS_FILTER="%~3"
-set RX=%~4
-set RY=%~5
-set SPU=%~6
-set ITER=%~7
-set THRESHOLD=%~8
-set TOOL=%~9
+set RENDER_DEVICE=%1
+set FILE_FILTER=%2
+set TESTS_FILTER="%3"
+set RX=%4
+set RY=%5
+set SPU=%6
+set ITER=%7
+set THRESHOLD=%8
+set TOOL=%9
 shift
-set ENGINE=%~9
+set ENGINE=%9
 
 if not defined RX set RX=0
 if not defined RY set RY=0
