066d77becbc04495b1231f82d88cb187068c944d
Tarasenko Daniil <dtarasenko@luxoft.com>
2020-05-26 15:46:02 +0300

    fix cis_tools def

--- scripts/run.bat
+++ scripts/run.bat
@@ -18,7 +18,7 @@ if not defined ITER set ITER=50
 if not defined THRESHOLD set THRESHOLD=0.05
 if not defined TOOL set TOOL=2020
 if not defined ENGINE set ENGINE=1
-if not defined CIS_TOOLS set CIS_TOOLS "C:\TestResources"
+if not defined CIS_TOOLS set CIS_TOOLS "C:\JN\cis_tools"
 
 IF not EXIST "%CIS_TOOLS%\..\TestResources\rpr_maya_autotests" (
     ECHO "rpr_maya_autotests assets don't exists"
