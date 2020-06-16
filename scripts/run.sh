0c921a003d1c0a43f3a25ec9e69f215cab858803
Evgenii Gurin <egurin@luxoft.com>
2020-06-16 20:26:28 +0300

    remove env setting

--- scripts/run.sh
+++ scripts/run.sh
@@ -10,24 +10,6 @@ THRESHOLD=${8:-0.05}
 TOOL=${9:-2020}
 ENGINE=${10:-1}
 
-shift
-shift
-shift
-shift
-shift
-shift
-shift
-shift
-shift
-shift
-
-export RBS_BUILD_ID=$1
-export RBS_JOB_ID=$2
-export RBS_URL=$3
-export RBS_ENV_LABEL=$4
-export IMAGE_SERVICE_URL=$5
-export RBS_USE=$6
-
 python3 -m pip install -r ../jobs_launcher/install/requirements.txt
 
 python3 ../jobs_launcher/executeTests.py --file_filter $FILE_FILTER --test_filter $TESTS_FILTER --tests_root ../jobs --work_root ../Work/Results --work_dir Maya --cmd_variables Tool "maya${TOOL}" RenderDevice "$RENDER_DEVICE" ResPath "$CIS_TOOLS/../TestResources/MayaAssets" PassLimit $ITER rx $RX ry $RY SPU $SPU threshold $THRESHOLD engine $ENGINE
