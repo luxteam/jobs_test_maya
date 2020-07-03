7e5eaf0e14bb5381eec2e241e5120cf6fbfe57cc
Andrey Alekseevich <magister.demens@gmail.com>
2020-07-03 15:59:56 +0300

    init

--- scripts/run.bat
+++ scripts/run.bat
@@ -13,11 +13,11 @@ set ENGINE=%9
 
 if not defined RX set RX=0
 if not defined RY set RY=0
-if not defined SPU set SPU=25
-if not defined ITER set ITER=50
-if not defined THRESHOLD set THRESHOLD=0.05
-if not defined TOOL set TOOL=2020
-if not defined ENGINE set ENGINE=1
+if not defined SPU set SPU=1
+if not defined ITER set ITER=1
+if not defined THRESHOLD set THRESHOLD=0.5
+if not defined TOOL set TOOL=2019
+if not defined ENGINE set ENGINE=2
 
 python -m pip install -r ..\jobs_launcher\install\requirements.txt
 
