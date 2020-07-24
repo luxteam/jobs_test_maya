37d9c4af25c062f82a21577a1d02f7e475dcea84
Piromancer <Den2000_09@mail.ru>
2020-07-24 15:43:09 +0300

    Shadow catcher group update

--- jobs/Scripts/extensions/Shadow_Catcher.py
+++ jobs/Scripts/extensions/Shadow_Catcher.py
@@ -1,4 +1,18 @@
 def alpha_render(case):
+    event('Prerender', False, case['case'])
+    logging('Render image')
+
+    mel.eval('fireRender -waitForItTwo')
+    start_time = time.time()
+    mel.eval('renderIntoNewWindow render')
+    cmds.sysFile(path.join(WORK_DIR, 'Color'), makeDir=True)
+    test_case_path = path.join(WORK_DIR, 'Color', case['case'])
+    cmds.renderWindowEditor('renderView', edit=1,  dst='color')
     mel.eval('renderWindowEditor -edit -displayStyle "mask" renderView')
-    rpr_render(case)
+    cmds.renderWindowEditor('renderView', edit=1, com=1,
+                            writeImage=test_case_path)
+    test_time = time.time() - start_time
+
+    event('Postrender', True, case['case'])
     mel.eval('renderWindowEditor -edit -displayStyle "color" renderView')
+    reportToJSON(case, test_time)
