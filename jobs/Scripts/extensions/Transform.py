1ac0ee7cde7fa41dc69cb8e968b8751cd0898cc1
PossessedKeepo <Possessedkeepo@gmail.com>
2020-10-09 17:12:27 +0300

    created Transform group, initial cases

---
+++ jobs/Scripts/extensions/Transform.py
@@ -0,0 +1,19 @@
+def resetAttributesIBL():
+    cmds.setAttr('RPRIBL.translateX', 0)
+    cmds.setAttr('RPRIBL.translateY', 0)
+    cmds.setAttr('RPRIBL.translateZ', 0)
+    cmds.setAttr('RPRIBL.rotateX', 0)
+    cmds.setAttr('RPRIBL.rotateY', 0)
+    cmds.setAttr('RPRIBL.rotateZ', 0)
+    cmds.setAttr('RPRIBL.scaleX', 1001.257)
+    cmds.setAttr('RPRIBL.scaleY', 1001.257)
+    cmds.setAttr('RPRIBL.scaleZ', 1001.257)
+    cmds.setAttr('RPRIBL.shearXY', 0)
+    cmds.setAttr('RPRIBL.shearXZ', 0)
+    cmds.setAttr('RPRIBL.shareYZ', 0)
+    cmds.setAttr('RPRIBL.rotateOrder', 0)
+    cmds.setAttr('RPRIBL.rotateAxisX', 0)
+    cmds.setAttr('RPRIBL.rotateAxisY', 0)
+    cmds.setAttr('RPRIBL.inheritsTransform', 1)
+
+
