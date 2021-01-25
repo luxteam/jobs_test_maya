8080f8a3669d2415fb6cc269abd2978fb13fd157
Andrei Kravchenko <epozine@inbox.ru>
2021-01-25 21:27:21 +0300

    Change assets path

--- scripts/build_rpr_cache.sh
+++ scripts/build_rpr_cache.sh
@@ -5,4 +5,4 @@ export MAYA_CMD_FILE_OUTPUT=$PWD/../Work/Results/Maya
 
 TOOL=${1:-2020}
 
-/Applications/Autodesk/Maya${TOOL}/Maya.app/Contents/bin/Render -r FireRender -log "$MAYA_CMD_FILE_OUTPUT/renderTool.cb.log" -rd "$MAYA_CMD_FILE_OUTPUT" -im "cache_building" -of jpg "$CIS_TOOLS/../TestResources/MayaAssets/material_baseline.mb"
+/Applications/Autodesk/Maya${TOOL}/Maya.app/Contents/bin/Render -r FireRender -log "$MAYA_CMD_FILE_OUTPUT/renderTool.cb.log" -rd "$MAYA_CMD_FILE_OUTPUT" -im "cache_building" -of jpg "$CIS_TOOLS/../TestResources/rpr_maya_autotests/material_baseline.mb"
